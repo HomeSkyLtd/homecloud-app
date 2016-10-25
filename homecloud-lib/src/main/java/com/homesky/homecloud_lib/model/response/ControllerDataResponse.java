c6ded4c0ab24128ce05ebe6b9eeb806ff31408ee
Fabio Muramatsu <fabio.muramatsu@outlook.com>
2016-10-25 18:23:43 +0000

    Added method to retrieve controller ids from ControllerDataResponse

--- homecloud-lib/src/main/java/com/homesky/homecloud_lib/model/response/ControllerDataResponse.java
+++ homecloud-lib/src/main/java/com/homesky/homecloud_lib/model/response/ControllerDataResponse.java
@@ -57,4 +57,8 @@ public class ControllerDataResponse extends SimpleResponse {
         }
         writer.endArray();
     }
+
+    public List<String> getControllerIds() {
+        return mControllerIds;
+    }
 }
