f645fcc677b2de0c81d39011a48f49df320aeab1
Fabio Muramatsu <fabio.muramatsu@outlook.com>
2016-11-16 13:01:26 +0000

    Added custom and channel values to enums

--- homecloud-lib/src/main/java/com/homesky/homecloud_lib/model/enums/CommandCategoryEnum.java
+++ homecloud-lib/src/main/java/com/homesky/homecloud_lib/model/enums/CommandCategoryEnum.java
@@ -1,7 +1,8 @@
 package com.homesky.homecloud_lib.model.enums;
 
 public enum CommandCategoryEnum implements SingleValueEnum {
-    TOGGLE(1), TEMPERATURE(2), FAN(3), LIGHT_SWITCH(4), AC_MODE(5), LIGHT_INTENSITY(6), LIGHT_COLOR(7);
+    TOGGLE(1), TEMPERATURE(2), FAN(3), LIGHT_SWITCH(4), AC_MODE(5), LIGHT_INTENSITY(6), LIGHT_COLOR(7),
+    CUSTOM(8), CHANNEL(9);
 
     private long id;
 
--- homecloud-lib/src/main/java/com/homesky/homecloud_lib/model/enums/DataCategoryEnum.java
+++ homecloud-lib/src/main/java/com/homesky/homecloud_lib/model/enums/DataCategoryEnum.java
@@ -2,7 +2,7 @@ package com.homesky.homecloud_lib.model.enums;
 
 public enum DataCategoryEnum implements SingleValueEnum {
     TEMPERATURE(1), LUMINANCE(2), PRESENCE(3), HUMIDITY(4),
-    PRESSURE(5), WIND_SPEED(6), SMOKE(7);
+    PRESSURE(5), WIND_SPEED(6), SMOKE(7), CUSTOM(8);
 
     private long id;
 
